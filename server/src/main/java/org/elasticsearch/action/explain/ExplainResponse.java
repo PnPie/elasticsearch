6a9ac40061281ddc19cfe6a4778b9cc010e467db
Yu <yu.liu003@gmail.com>
2018-05-28 14:31:47 +0200

    Rest highlevel explain API
    Add explain API to rest high level client.

--- server/src/main/java/org/elasticsearch/action/explain/ExplainResponse.java
+++ server/src/main/java/org/elasticsearch/action/explain/ExplainResponse.java
@@ -21,11 +21,17 @@ package org.elasticsearch.action.explain;
 
 import org.apache.lucene.search.Explanation;
 import org.elasticsearch.action.ActionResponse;
+import org.elasticsearch.common.ParseField;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.xcontent.ConstructingObjectParser;
+import org.elasticsearch.common.xcontent.ToXContentObject;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.index.get.GetResult;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import static org.elasticsearch.common.lucene.Lucene.readExplanation;
 import static org.elasticsearch.common.lucene.Lucene.writeExplanation;
@@ -33,7 +39,17 @@ import static org.elasticsearch.common.lucene.Lucene.writeExplanation;
 /**
  * Response containing the score explanation.
  */
-public class ExplainResponse extends ActionResponse {
+public class ExplainResponse extends ActionResponse implements ToXContentObject {
+
+    private static final ParseField _INDEX = new ParseField("_index");
+    private static final ParseField _TYPE = new ParseField("_type");
+    private static final ParseField _ID = new ParseField("_id");
+    private static final ParseField MATCHED = new ParseField("matched");
+    private static final ParseField EXPLANATION = new ParseField("explanation");
+    private static final ParseField VALUE = new ParseField("value");
+    private static final ParseField DESCRIPTION = new ParseField("description");
+    private static final ParseField DETAILS = new ParseField("details");
+    private static final ParseField GET = new ParseField("get");
 
     private String index;
     private String type;
@@ -86,6 +102,10 @@ public class ExplainResponse extends ActionResponse {
         return explanation != null;
     }
 
+    public void setExists(boolean exists) {
+        this.exists = exists;
+    }
+
     public boolean isExists() {
         return exists;
     }
@@ -128,5 +148,67 @@ public class ExplainResponse extends ActionResponse {
             out.writeBoolean(true);
             getResult.writeTo(out);
         }
+    }
+
+    private static final ConstructingObjectParser<ExplainResponse, Void> PARSER = new ConstructingObjectParser<>("explain", true,
+        arg -> new ExplainResponse((String) arg[0], (String) arg[1], (String) arg[2], (boolean) arg[3], (Explanation) arg[4],
+            (GetResult) arg[5]));
+
+    static {
+        PARSER.declareString(ConstructingObjectParser.constructorArg(), _INDEX);
+        PARSER.declareString(ConstructingObjectParser.constructorArg(), _TYPE);
+        PARSER.declareString(ConstructingObjectParser.constructorArg(), _ID);
+        PARSER.declareBoolean(ConstructingObjectParser.constructorArg(), MATCHED);
+        final ConstructingObjectParser<Explanation, Void> explanationParser = new ConstructingObjectParser<>("explanation", true,
+            arg -> {
+                if ((float) arg[0] > 0) {
+                    return Explanation.match((float) arg[0], (String) arg[1], (Collection<Explanation>) arg[2]);
+                } else {
+                    return Explanation.noMatch((String) arg[1], (Collection<Explanation>) arg[2]);
+                }
+            });
+        explanationParser.declareFloat(ConstructingObjectParser.constructorArg(), VALUE);
+        explanationParser.declareString(ConstructingObjectParser.constructorArg(), DESCRIPTION);
+        explanationParser.declareObjectArray(ConstructingObjectParser.constructorArg(), explanationParser, DETAILS);
+        PARSER.declareObject(ConstructingObjectParser.optionalConstructorArg(), explanationParser, EXPLANATION);
+        PARSER.declareObject(ConstructingObjectParser.optionalConstructorArg(), (p, c) -> GetResult.fromXContentEmbedded(p), GET);
+    }
+
+    public static ExplainResponse fromXContent(XContentParser parser) {
+        return PARSER.apply(parser, null);
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject();
+        builder.field(_INDEX.getPreferredName(), index);
+        builder.field(_TYPE.getPreferredName(), type);
+        builder.field(_ID.getPreferredName(), id);
+        builder.field(MATCHED.getPreferredName(), isMatch());
+        if (hasExplanation()) {
+            builder.startObject(EXPLANATION.getPreferredName());
+            builder.field(VALUE.getPreferredName(), explanation.getValue());
+            builder.field(DESCRIPTION.getPreferredName(), explanation.getDescription());
+            detailsToXContect(builder, params, explanation.getDetails());
+            builder.endObject();
+        }
+        if (getResult != null) {
+            builder.startObject(GET.getPreferredName());
+            getResult.toXContentEmbedded(builder, params);
+            builder.endObject();
+        }
+        return builder;
+    }
+
+    private void detailsToXContect(XContentBuilder builder, Params params, Explanation[] explanations) throws IOException {
+        builder.startArray(DETAILS.getPreferredName());
+        for (Explanation explanation : explanations) {
+            builder.startObject();
+            builder.field(VALUE.getPreferredName(), explanation.getValue());
+            builder.field(DESCRIPTION.getPreferredName(), explanation.getDescription());
+            detailsToXContect(builder, params, explanation.getDetails());
+            builder.endObject();
+        }
+        builder.endArray();
     }
 }
